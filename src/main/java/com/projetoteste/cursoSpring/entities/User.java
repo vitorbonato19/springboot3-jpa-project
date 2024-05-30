1d1d1920079b9affdc3d1934b3a241ea51f4cd70
Vitor Bonato <vitorbonatocam@gmail.com>
2024-05-30 10:55:27 -0300

    EndPoint and User mapped

--- src/main/java/com/projetoteste/cursoSpring/entities/User.java
+++ src/main/java/com/projetoteste/cursoSpring/entities/User.java
@@ -1,9 +1,11 @@
 package com.projetoteste.cursoSpring.entities;
 
+import java.io.Serializable;
 import java.util.Objects;
 
-public class User {
+public class User implements Serializable {
 
+	private static final long serialVersionUID = 1L;
 	
 	private Long id;
 	private String name;
---
+++ src/main/java/com/projetoteste/cursoSpring/resources/UserResource.java
@@ -0,0 +1,20 @@
+package com.projetoteste.cursoSpring.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.projetoteste.cursoSpring.entities.User;
+
+@RestController
+@RequestMapping({"/users"})
+public class UserResource {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User u = new User(1L, "Maria", "maria@gmail.com", "99999", "23451");
+		return ResponseEntity.ok().body(u);
+	}
+	
+}
