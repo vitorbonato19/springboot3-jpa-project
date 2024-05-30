706c5f4023cf78843fc8a52adbaf965ac407a79f
Vitor Bonato <vitorbonatocam@gmail.com>
2024-05-30 11:58:29 -0300

    Service layer, component registration

--- src/main/java/com/projetoteste/cursoSpring/repositories/UserRepository.java
+++ src/main/java/com/projetoteste/cursoSpring/repositories/UserRepository.java
@@ -1,11 +1,11 @@
 package com.projetoteste.cursoSpring.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.projetoteste.cursoSpring.entities.User;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long>{
 	
-	
-
 }
