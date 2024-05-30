bd1654a48368b085506c0cf044756371df2febd0
Vitor Bonato <vitorbonatocam@gmail.com>
2024-05-30 10:34:14 -0300

    Project Created

---
+++ src/test/java/com/projetoteste/cursoJava/CursoJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetoteste.cursoJava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
