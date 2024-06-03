0282929c0e72e09fc44cf4c6e5076375dcc8fe6d
Vitor Bonato <vitorbonatocam@gmail.com>
2024-06-02 23:25:30 -0300

    Many to Many association with JoinTable

---
+++ src/main/java/com/projetoteste/cursoSpring/services/ProductService.java
@@ -0,0 +1,27 @@
+package com.projetoteste.cursoSpring.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.projetoteste.cursoSpring.entities.Product;
+import com.projetoteste.cursoSpring.repositories.ProductRepository;
+
+@Service
+public class ProductService {
+
+	@Autowired
+	private ProductRepository productRepository;
+	
+	public List<Product> findAll(){
+		return productRepository.findAll();
+	}
+	
+	public Product findById(Long id) {
+		Optional<Product> obj = productRepository.findById(id);
+		return obj.get();
+	}
+	
+}
