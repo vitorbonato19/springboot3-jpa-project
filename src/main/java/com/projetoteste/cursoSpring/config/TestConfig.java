6736fa0bd067d8c22e5482dc86ffc236786a577b
Vitor Bonato <vitorbonatocam@gmail.com>
2024-06-02 22:46:34 -0300

    Category Controller added

--- src/main/java/com/projetoteste/cursoSpring/config/TestConfig.java
+++ src/main/java/com/projetoteste/cursoSpring/config/TestConfig.java
@@ -8,9 +8,11 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import com.projetoteste.cursoSpring.entities.Category;
 import com.projetoteste.cursoSpring.entities.Order;
 import com.projetoteste.cursoSpring.entities.User;
 import com.projetoteste.cursoSpring.entities.enums.OrderStatus;
+import com.projetoteste.cursoSpring.repositories.CategoryRepository;
 import com.projetoteste.cursoSpring.repositories.OrderRepository;
 import com.projetoteste.cursoSpring.repositories.UserRepository;
 
@@ -23,19 +25,29 @@ public class TestConfig implements CommandLineRunner {
 	
 	@Autowired
 	private OrderRepository orderRepository;
+	
+	@Autowired
+	private CategoryRepository categoryRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
+		
+		Category cat1 = new Category(null, "Electronics");
+		Category cat2 = new Category(null, "Books");
+		Category cat3 = new Category(null, "Computers");
+		
+		categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
 		
 		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "890128358", "1230968");
 		User u2 = new User(null, "Carlos White", "carlos@gmail.com", "535481348", "901283");
 		User u3 = new User(null, "Matheus da Silva" , "matheus.silva@gmai.com", "46741546", "132467");
 		
+		userRepository.saveAll(Arrays.asList(u1,u2,u3));
+		
 		Order o1 = new Order(null, Instant.parse("2024-05-29T20:23:06Z"), OrderStatus.PAID, u1); 
 		Order o2 = new Order(null, Instant.parse("2024-05-30T19:19:03Z"), OrderStatus.WAITING_PAYMENT, u2);
 		Order o3 = new Order(null, Instant.parse("2024-05-31T22:46:02Z"), OrderStatus.WAITING_PAYMENT, u3);
 		
-		userRepository.saveAll(Arrays.asList(u1,u2,u3));
 		orderRepository.saveAll(Arrays.asList(o1,o2,o3));
 	}
 	
---
+++ src/main/java/com/projetoteste/cursoSpring/services/CategoryService.java
@@ -0,0 +1,27 @@
+package com.projetoteste.cursoSpring.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.projetoteste.cursoSpring.entities.Category;
+import com.projetoteste.cursoSpring.repositories.CategoryRepository;
+
+@Service
+public class CategoryService {
+
+	@Autowired
+	private CategoryRepository categoryRepository;
+	
+	public List<Category> findAll(){
+		return categoryRepository.findAll();
+	}
+	
+	public Category findById(Long id) {
+		Optional<Category> obj = categoryRepository.findById(id);
+		return obj.get();
+	}
+	
+}
