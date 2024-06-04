8a8f39e4fd7cbd41a96745b19677ff6b3468b14f
Vitor Bonato <vitorbonatocam@gmail.com>
2024-06-03 22:08:12 -0300

    User Controller, PUT Method HTTP

--- src/main/java/com/projetoteste/cursoSpring/services/UserService.java
+++ src/main/java/com/projetoteste/cursoSpring/services/UserService.java
@@ -31,4 +31,17 @@ public class UserService {
 	public void delete(Long id) {
 		userRepository.deleteById(id);
 	}
+	
+	public User udpate(Long id, User user) {
+		User entity = userRepository.getReferenceById(id);
+		updateData(entity, user);
+		return userRepository.save(entity);
+	}
+
+	private void updateData(User entity, User user) {
+		entity.setName(user.getName());
+		entity.setEmail(user.getEmail());
+		entity.setPhone(user.getPhone());
+		
+	}
 }
